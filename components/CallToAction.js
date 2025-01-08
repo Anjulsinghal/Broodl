06cd1ceb6a5f849949592af305f05f7be0c71094
Anjulsinghal <anjulsinghal123@gmail.com>
2025-01-09 02:44:20 +0530

    nothing

--- components/CallToAction.js
+++ components/CallToAction.js
@@ -36,13 +36,14 @@ export default function CallToAction() {
     )
   }
   return (
-    <div className='grid grid-cols-2 gap-4 w-fit mx-auto'>
+    <div className='grid grid-cols-3 gap-4 w-fit mx-auto'>
         <Link href={'/Dashboard'}>
           <Button text="Login"/>
         </Link>
         <Link href={'/Dashboard'}>
           <Button text="Sign_up" dark/>
         </Link>
+        <Button text="Demo-AC" clickHandler={handleDemoClick} light />
         
       </div>
   )
